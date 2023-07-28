617ba0788e9b6451369c48fac88229fa819062ca
c.morais <c.morais1507@gmail.com>
2023-07-28 01:07:43 -0300

    fix: Minor Issues

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,10 +2,19 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
+  try {
+    const users = await prisma.user.findMany();
 
-  const users = await prisma.user.findMany();
+    console.log(users);
 
-  console.log(users);
+    return NextResponse.json(users);
+  } catch (error) {
+    // Log the error
+    console.error("Error occurred while querying the database:", error);
 
-  return NextResponse.json(users)
+    // Return an error response with a custom status code
+    return new NextResponse("An error occurred while fetching users.", {
+      status: 500,
+    });
+  }
 }
