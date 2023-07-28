1fd39747ffd5660be26911b173ff7847b6d08399
c.morais <c.morais1507@gmail.com>
2023-07-28 00:56:24 -0300

    fix: Minor Issues

---
+++ app/blog/page.tsx
@@ -0,0 +1,8 @@
+export default async function Blog() {
+  return (
+    <div>
+      <h1 className="font-bold text-2xl">Oi! Então...</h1>
+      <h2 className="text-xl">Nós não temos um blog por agora :/</h2>
+    </div>
+  )
+}
