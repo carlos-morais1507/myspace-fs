22127e450e233c0781e64dcdd6bd8a6d9e90a01d
c.morais <c.morais1507@gmail.com>
2023-07-28 01:41:52 -0300

    fix: Minor Fixes

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -5,14 +5,10 @@ export async function GET(request: Request) {
   try {
     const users = await prisma.user.findMany();
 
-    console.log(users);
-
     return NextResponse.json(users);
   } catch (error) {
-    // Log the error
     console.error("Error occurred while querying the database:", error);
 
-    // Return an error response with a custom status code
     return new NextResponse("An error occurred while fetching users.", {
       status: 500,
     });
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,11 +2,11 @@ import UserCard from "@/components/UserCard";
 import { prisma } from "@/lib/prisma";
 
 export default async function Users() {
-  const users = await prisma.user.findMany();
+  const res = await fetch('https://myspace-fs.vercel.app/api/users').then((res) => res.json())
 
   return (
     <div className="flex gap-3 flex-wrap items-center justify-center">
-      {users.map((user) => {
+      {res.map((user: any) => {
         return <UserCard key={user.id} {...user} />
       })}
     </div>
