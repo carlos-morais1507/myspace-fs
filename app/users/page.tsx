fd40ef798e8234f78ede87f8078d2c27cf2a18f6
c.morais <c.morais1507@gmail.com>
2023-07-28 01:48:46 -0300

    fix: Minor Fixes

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,12 +1,13 @@
 import UserCard from "@/components/UserCard";
 import { prisma } from "@/lib/prisma";
 
+
 export default async function Users() {
-  const res = await fetch('https://myspace-fs.vercel.app/api/users').then((res) => res.json())
+  const users = await prisma.user.findMany();
 
   return (
     <div className="flex gap-3 flex-wrap items-center justify-center">
-      {res.map((user: any) => {
+      {users.map((user: any) => {
         return <UserCard key={user.id} {...user} />
       })}
     </div>
